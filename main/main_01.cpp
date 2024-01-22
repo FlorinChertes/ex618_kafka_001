c33f2f0aedd55a30b2f6f1302227d50718eedb14
Florin Chertes <FlorinChertes@computer.org>
2024-01-22 10:55:54 +0100

    first commit

---
+++ main/main_01.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <string>
+
+
+int main ([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
+{
+	std::cout << "main ex618_kafka_001" << std::endl;
+
+	return 0;
+}
