101d55bba7d6d8892164087c062ae401511b318d
Florin Chertes <FlorinChertes@computer.org>
2024-01-28 21:05:09 +0100

    revert to metadata.broker.list

--- main/main_01.cpp
+++ main/main_01.cpp
@@ -14,13 +14,27 @@ int main ([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
 	std::string brokers = "localhost:9092";
     std::string topicName = "amazingTopic";
 
+	std::cout << "input brocker address: " << std::endl;
+    std::string brocker_address{};
+    std::getline(std::cin, brocker_address);
+    if (brocker_address.empty() == false)
+    {
+      brokers =  brocker_address;
+    }
+
+    std::cout << "input brocker address: >" << brokers << "<" << std::endl;
+
     RdKafka::Conf *conf = RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL);
-    conf->set("bootstrap-server", brokers, errstr);
+    std::cout << "configuration created" << std::endl;
+    conf->set("metadata.broker.list", brokers, errstr);
+    std::cout << "brocker name set in conf" << std::endl;
 
     RdKafka::Producer *producer = RdKafka::Producer::create(conf, errstr);
     delete conf;
+    std::cout << "producer created using conf, conf deleted" << std::endl;
 
     RdKafka::Topic *topic = RdKafka::Topic::create(producer, topicName, nullptr, errstr);
+    std::cout << "topic created using producer" << std::endl;
 
     for (std::string line; std::getline(std::cin, line);)
 	{
@@ -30,6 +44,7 @@ int main ([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
                           const_cast<char *>(line.c_str()), line.size(),
                           nullptr, nullptr);
     }
+   std::cout << "sendind loop exited" << std::endl;
 
     producer->flush(10*1000 /* wait for max 10 seconds */);
 
