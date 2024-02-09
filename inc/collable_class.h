67c3f2f86e4cb3b4e72faeceb38bb012623d8141
Florin Chertes <FlorinChertes@computer.org>
2024-02-09 15:20:56 +0100

    change in collable

--- inc/collable_class.h
+++ inc/collable_class.h
@@ -1,33 +1,20 @@
 #pragma once
 
-template<typename F, typename... Args>
+template<typename F, typename Arg>
 class CollableClass
 {
     F func_;
-    std::tuple<Args&...> args_;
+    Arg& arg_;
 
 public:
     // Constructor to initialize the function object and arguments
-    CollableClass(F&& func, Args&... args)
+    CollableClass(F&& func, Arg& arg)
         : func_{ std::move(func) }
-        , args_{ std::forward<Args&>(args)... }
+        , arg_{ arg }
     {}
 
     void invoke()
     {
-        apply(func_, args_);
-    }
-
-private:
-    // Helper function to invoke the function object with arguments from a tuple
-    template<typename Function, typename Tuple, size_t... Index>
-    void apply_impl(Function&& func, Tuple&& tuple, std::index_sequence<Index...>) {
-        func(std::get<Index>(std::forward<Tuple>(tuple))...);
-    }
-
-    template<typename Function, typename Tuple>
-    void apply(Function&& func, Tuple&& tuple) {
-        constexpr size_t size = std::tuple_size<std::remove_reference_t<Tuple>>::value;
-        apply_impl(std::forward<Function>(func), std::forward<Tuple>(tuple), std::make_index_sequence<size>{});
+        func_(arg_);
     }
 };
