19fe1c67c0211dfabd3171d6c3a65d9967e56e04
FlorinChertes <FlorinChertes@computer.org>
2024-02-09 09:14:46 +0100

    using jthread in BackgroundWoerker

--- inc/background_worker.h
+++ inc/background_worker.h
@@ -16,35 +16,16 @@ public:
         workerThread = std::jthread(&BackgroundWorker::workerFunction, this);
     }
 
-    ~BackgroundWorker() {
-        // Signal the worker thread to stop
-        stopSignal = true;
-
-        // Join the worker thread in the destructor to ensure it completes before the object is destroyed
-        if (workerThread.joinable()) {
-            workerThread.join();
-        }
-    }
-
-    // Deleted copy constructor and assignment operator to prevent copying
     BackgroundWorker(const BackgroundWorker&) = delete;
     BackgroundWorker& operator=(const BackgroundWorker&) = delete;
 
-    // Optionally, define move constructor and assignment operator if needed
-
 private:
     std::jthread workerThread;
-    std::atomic_bool stopSignal {false};
-
     T& callable_;
 
     void workerFunction() {
-        while (!stopSignal) {
-            std::cout << "Working..." << std::endl;
-
-            callable_.invoke();
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-        }
+        std::cout << "Working..." << std::endl;
+        callable_.invoke();
         std::cout << "Worker thread stopping." << std::endl;
     }
 };
